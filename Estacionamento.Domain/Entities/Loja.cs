c531832f569b8ec840167739b203371ffabcedf4
codelize <lflizier@gmail.com>
2025-04-26 20:35:41 -0300

    feat: criação da entidade Loja

---
+++ Estacionamento.Domain/Entities/Loja.cs
@@ -0,0 +1,19 @@
+﻿namespace Estacionamento.Domain.Entities;
+
+public class Loja
+{
+    public Guid Id { get; private set; }
+    public string Nome { get; private set; }
+    public string Setor { get; private set; }
+    public int CoordenadaX { get; private set; }
+    public int CoordenadaY { get; private set; }
+
+    public Loja(string nome, string setor, int coordenadaX, int coordenadaY)
+    {
+        Id = Guid.NewGuid();
+        Nome = nome;
+        Setor = setor;
+        CoordenadaX = coordenadaX;
+        CoordenadaY = coordenadaY;
+    }
+}
