af731dd03146afeb19a61b22f901d8a04a05fcb7
codelize <lflizier@gmail.com>
2025-04-27 11:20:35 -0300

    feat: criação do endpoint de listagem de vagas disponíveis

---
+++ Estacionamento.API/Endpoints/ListarVagasDisponiveisEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using FastEndpoints;
+using Estacionamento.Infrastructure.Persistence;
+using Estacionamento.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Estacionamento.API.Endpoints;
+
+    public class ListarVagasDisponiveisEndpoint : EndpointWithoutRequest<List<Vaga>>
+    {
+        private readonly EstacionamentoDbContext _context;
+
+        public ListarVagasDisponiveisEndpoint(EstacionamentoDbContext context)
+        {
+             _context = context;    
+        }
+
+        public override void Configure()
+        {
+            Get("/vagas/disponiveis"); // URL nova para listar só vagas livres 
+            AllowAnonymous();
+        }
+
+        public override async Task HandleAsync(CancellationToken ct)
+        {
+            var vagasDisponiveis = await _context.Vagas
+                .Where(v => v.Disponivel) // Só vagas onde Disponível == true
+                .ToListAsync(ct);
+
+            await SendAsync(vagasDisponiveis);
+        
+    }
+}
