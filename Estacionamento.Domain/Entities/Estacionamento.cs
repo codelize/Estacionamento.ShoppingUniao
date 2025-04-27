6a1f3188db42f66c0594164f2dfe5f86c5e902e2
codelize <lflizier@gmail.com>
2025-04-26 21:10:44 -0300

    feat: criação da entidade Estacionamento

---
+++ Estacionamento.Domain/Entities/Estacionamento.cs
@@ -0,0 +1,33 @@
+﻿using Estacionamento.Domain.Entities;
+
+namespace Estacionamento.Domain.Entities;
+
+public class Estacionamento
+{
+    public Guid Id { get; private set; }
+    public string Nome { get; private set; }
+    public List<string> Setores { get; private set; }
+    public List<Vaga> Vagas { get; private set; }
+    public List<Loja> Lojas { get; private set; }
+
+    public Estacionamento(string nome, List<string> setores)
+    {
+        Id = Guid.NewGuid();
+        Nome = nome;
+        Setores = setores ?? new List<string>();
+        Vagas = new List<Vaga>();
+        Lojas = new List<Loja>();
+    }
+
+    public void AdicionarVaga(Vaga vaga)
+    {
+        if (vaga != null)
+            Vagas.Add(vaga);
+    }
+
+    public void AdicionarLoja(Loja loja)
+    {
+        if (loja != null)
+        Lojas.Add(loja);
+    }
+}
